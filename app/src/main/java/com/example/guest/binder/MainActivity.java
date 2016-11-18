3f5f04dc2cf742747fb1ac5314911dfda06fc13c
Garrett Boggs <garrett.boggs@my.wheaton.edu>
2016-11-18 08:08:19 -0800

    allow user to navigate to cover rating page

--- app/src/main/java/com/example/guest/binder/MainActivity.java
+++ app/src/main/java/com/example/guest/binder/MainActivity.java
@@ -1,13 +1,36 @@
 package com.example.guest.binder;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Bind(R.id.loginButton) Button mLoginButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        mLoginButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v){
+        Intent intent = new Intent(MainActivity.this, CoverActivity.class);
+
+        if(v == mLoginButton) {
+          startActivity(intent);
+            Toast.makeText(MainActivity.this, "Welcome!", Toast.LENGTH_LONG).show();
+        }
     }
 }
