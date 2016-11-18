04d9beff52eed4d3898b5142ea6982e612959751
Garrett Boggs <garrett.boggs@my.wheaton.edu>
2016-11-18 09:05:55 -0800

    allow user to select 'cool' or 'lame' for book cover

--- app/src/main/java/com/example/guest/binder/CoverActivity.java
+++ app/src/main/java/com/example/guest/binder/CoverActivity.java
@@ -1,13 +1,41 @@
 package com.example.guest.binder;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class CoverActivity extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class CoverActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Bind(R.id.coolButton) Button mCoolButton;
+    @Bind(R.id.lameButton) Button mLameButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cover);
+        ButterKnife.bind(this);
+
+        mCoolButton.setOnClickListener(this);
+        mLameButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v){
+        Intent intent = new Intent(CoverActivity.this, StatsActivity.class);
+
+        if(v == mCoolButton){
+
+            startActivity(intent);
+        }
+
+        if(v == mLameButton){
+
+            startActivity(intent);
+        }
     }
 }
