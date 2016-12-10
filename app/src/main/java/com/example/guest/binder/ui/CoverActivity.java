b169797e2a92a3f74fb435d9669dfebcc948f174
Garrett Boggs <garrett.boggs@my.wheaton.edu>
2016-12-10 13:40:19 -0800

    more layout changes

--- app/src/main/java/com/example/guest/binder/ui/CoverActivity.java
+++ app/src/main/java/com/example/guest/binder/ui/CoverActivity.java
@@ -90,7 +90,7 @@ public class CoverActivity extends AppCompatActivity implements View.OnClickList
 
                     mCharacters.add(temp);
 
-                    if(mCharacters.size() > 35 && !start){
+                    if(mCharacters.size() > 40 && !start){
                         int guess = rn.nextInt(mCharacters.size());
                         mCharacterOne = mCharacters.get(guess);
                         mCharacterOneButton.setText(mCharacterOne.getName());
